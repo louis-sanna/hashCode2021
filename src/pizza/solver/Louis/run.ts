609940d729b0bd2c1ac82728398d5364d4593259
Julien Bara <julien.bara@360learning.com>
2021-02-24 20:06:21 +0100

    fix import

--- src/pizza/solver/Louis/run.ts
+++ src/pizza/solver/Louis/run.ts
@@ -2,7 +2,7 @@ import { parseInput } from './../../parser';
 import { score } from './../../scorer';
 import { solve } from './solve';
 import { log } from './../../logger';
-import { Input, Submission } from './../../Entities';
+import { Input, Submission } from './../../entities';
 
 console.log('Start solving HASH CODE 2021!');
 
