89edbe6416003405d7977602450372a06d13d258
Julien Bara <julien.bara@360learning.com>
2021-02-24 21:58:00 +0100

    Julien - Monte Carlo

--- src/pizza/solver/Julien/run.ts
+++ src/pizza/solver/Julien/run.ts
@@ -9,9 +9,18 @@ console.log('Start solving HASH CODE 2021!');
 const problem = 'A';
 
 const parsedInput : Input = parseInput(problem);
-const submission : Submission = solve(parsedInput);
-const totalScore : number = score(submission, parsedInput);
+let bestScore  = 0;
 
-console.log(`For problem ${problem}, we found a solution scored: ${totalScore}`);
+// eslint-disable-next-line no-constant-condition
+while(true){
+    const submission : Submission = solve(parsedInput);
+    const totalScore : number = score(submission, parsedInput);
 
-log(submission, problem, totalScore);
+    if(totalScore > bestScore){
+        console.log(`For problem ${problem}, we found a solution scored: ${totalScore}`);
+
+        log(submission, problem, totalScore);
+        bestScore = totalScore;
+    }
+}
+
--- src/pizza/solver/Julien/solve.ts
+++ src/pizza/solver/Julien/solve.ts
@@ -1,9 +1,43 @@
+import _ from 'underscore';
 import { Input, Submission } from '../../entities';
 
 export function solve(input: Input) : Submission {
-    return {
-        deliveries: [{
-            pizzaIndexes: [0,1,2,3]
-        }]
-    };
+    const pizzas = input.pizzas;
+    const pizzasLeftIndexes = _.shuffle(range(pizzas.length));
+    let {numberOfTwoPersonTeams, numberOfThreePersonTeams, numberOfFourPersonTeams} = input;
+    const submission: Submission =  { deliveries: [] };
+
+    while(pizzasLeftIndexes.length >= 2
+        && (numberOfTwoPersonTeams !== 0
+             || numberOfThreePersonTeams !== 0
+             || numberOfFourPersonTeams !== 0)){
+
+        const strategy = _.sample([2,3,4]);
+
+        switch (strategy) {
+        case 2:
+            if(pizzasLeftIndexes.length >= 2 && numberOfTwoPersonTeams !== 0){
+                submission.deliveries.push({pizzaIndexes: pizzasLeftIndexes.splice(0,2)});
+                numberOfTwoPersonTeams--;
+            }
+            continue;
+        case 3:
+            if(pizzasLeftIndexes.length >= 3 && numberOfThreePersonTeams !== 0){
+                submission.deliveries.push({pizzaIndexes: pizzasLeftIndexes.splice(0,3)});
+                numberOfThreePersonTeams--;
+            }
+            continue;
+        case 4:
+            if(pizzasLeftIndexes.length >= 4 && numberOfFourPersonTeams !== 0){
+                submission.deliveries.push({pizzaIndexes: pizzasLeftIndexes.splice(0,4)});
+                numberOfFourPersonTeams--;
+            }
+            continue;
+        }
+    }
+
+    return submission;
 }
+
+// Python's `range` method
+const range = n => Array.from(Array(n).keys());
