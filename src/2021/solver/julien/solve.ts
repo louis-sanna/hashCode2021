88f0a8c9e2aae9e6a65dd5f895208e4b28fb8e72
Julien Bara <julien.bara@360learning.com>
2021-02-25 21:06:24 +0100

    add remove unused output

--- src/2021/solver/julien/solve.ts
+++ src/2021/solver/julien/solve.ts
@@ -2,13 +2,20 @@ import _ from 'underscore';
 import { Input, IntersectionSchedule, Submission } from '../../entities';
 
 export function solve(input: Input) : Submission {
+    // remove unused output
+    const usedStreets : Set<string> = new Set(_.flatten(input.cars.map(car => car.pathStreetNames)));
+    for (const id in input.intersectionsById) {
+        input.intersectionsById[id].inputStreets.filter(street => usedStreets.has(street));
+    }
+
+
     const intersectionSchedules : Array<IntersectionSchedule> = Object.entries(input.intersectionsById).map(
         ([id, intersection]) => ({
             intersection: parseInt(id, 10),
-            schedules: [{
-                duration: 1,
-                street: intersection.inputStreets[0]
-            }]
+            schedules: intersection.inputStreets.map(street => ({
+                street: street,
+                duration: 1
+            }))
         })
     );
 
