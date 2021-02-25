00917be358426b7eb348d4c36413fb07c23067d6
Louis Sanna <louis.sanna@360learning.com>
2021-02-25 19:28:20 +0100

    add first test on scorer

--- src/2021/scorer/score.test.ts
+++ src/2021/scorer/score.test.ts
@@ -11,4 +11,58 @@ describe('score', () => {
 
         expect(totalScore).to.equal(0);
     });
+
+    it('should return 0 input there are no cars', () => {
+        const input = {
+            duration: 0,
+            intersectionsCount: 0,
+            streetsCount: 0,
+            carsCount: 0,
+            bonus: 0,
+            streets: [{
+                begin: 0,
+                end: 1,
+                name: 'name of street',
+                length: 1
+            }],
+            cars: [{
+                pathLength: 1,
+                pathStreetNames: ['name of street']
+            }],
+            // custom
+            intersectionsById: {
+                '0':
+                {
+                    inputStreets: [],
+                    outputStreets: ['name of street']
+                },
+                '1':
+                {
+                    inputStreets: ['name of street'],
+                    outputStreets: []
+                }
+
+            },
+            streetsByName: {'name of street':
+            {
+                begin: 0,
+                end: 1,
+                name: 'name of street',
+                length: 1
+            }}
+        };
+        const submission = {
+            intersectionSchedules: [{
+                intersection: 1,
+                schedules: [{
+                    street: 'name of street',
+                    duration: 0
+                }]
+            }]
+        };
+
+        const totalScore = score(submission, input);
+
+        expect(totalScore).to.equal(0);
+    });
 });
