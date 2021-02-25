f7ead424a82ab4deaec9f63616f97ead04181b96
sebastien-mignot <mignot.sebastien@gmail.com>
2021-02-25 19:54:37 +0100

    proposition for usable light schedule

--- src/2021/entities/Traffic.ts
+++ src/2021/entities/Traffic.ts
@@ -8,3 +8,11 @@ export interface TrafficState {
     time: number,
     carPositions: Array<CarPosition>
 }
+
+export interface FullLightsSchedule {
+    stateByTime: Array<LightsState>; //the index in this array is the time
+}
+
+export interface LightsState {
+    lightsByIntersection: Array<string>; //the index in this array is the intersection index, and the string is the street currently green
+}
