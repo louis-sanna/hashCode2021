6015025aec5919adfeacda84d00797fbe1240b14
Louis Sanna <louis.sanna@360learning.com>
2021-02-25 21:20:30 +0100

    Add scorer

--- src/2021/scorer/score.ts
+++ src/2021/scorer/score.ts
@@ -1,6 +1,32 @@
-import { Input, Submission } from '../entities';
+import { Input, Submission, TrafficState } from '../entities';
+import computeFullLightsSchedule from './computeFullLightsSchedule';
+import updateTrafficState from './updateTrafficState';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function score(submission: Submission, input: Input): number {
-    return 0;
+    const fullLightsSchedule = computeFullLightsSchedule(input, submission);
+    let totalScore = 0;
+    const finalTime = input.duration;
+    const trafficState = generateTrafficState();
+    while (trafficState.time < finalTime) {
+        updateTrafficState(trafficState, fullLightsSchedule, input);
+        const currentTime = trafficState.time;
+        trafficState.carsArrived.forEach(() => {
+            totalScore += input.bonus;
+            totalScore += finalTime - trafficState.time;
+        });
+        trafficState.carsArrived = [];
+    }
+    return totalScore;
+}
+
+function generateTrafficState(input: Input) : TrafficState {
+    return {
+        time: 0,
+        carPositions: input.cars.map(({ id, pathStreetNames }) => ({
+            street: pathStreetNames[0],
+            car: id,
+            distanceToEnd: 0
+        })),
+        carsArrived: []
+    };
 }
