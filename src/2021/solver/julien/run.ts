ef27f8a2006e210661de0d86946fb07f32ecb3b1
Julien Bara <julien.bara@360learning.com>
2021-02-25 18:44:45 +0100

    julien solver

---
+++ src/2021/solver/julien/run.ts
@@ -0,0 +1,25 @@
+import { parseInput } from '../../parser';
+import { score } from '../../scorer';
+import { log } from '../../logger';
+import { Input, Submission } from '../../entities';
+import { solve } from './solve';
+
+console.log('Start solving HASH CODE 2021!');
+
+const problem = 'A';
+
+const parsedInput : Input = parseInput(problem);
+const bestScore  = 0;
+
+// eslint-disable-next-line no-constant-condition
+// while(true){
+const submission : Submission = solve(parsedInput);
+const totalScore : number = score(submission, parsedInput);
+
+// if(totalScore > bestScore){
+console.log(`For problem ${problem}, we found a solution scored: ${totalScore}`);
+
+log(submission, problem, totalScore);
+// bestScore = totalScore;
+// }
+// }
---
+++ src/2021/solver/julien/solve.ts
@@ -0,0 +1,11 @@
+import _ from 'underscore';
+import { Input, Submission } from '../../entities';
+
+export function solve(input: Input) : Submission {
+    const x : Submission = {};
+
+    return x;
+}
+
+// Python's `range` method
+const range = n => Array.from(Array(n).keys());
