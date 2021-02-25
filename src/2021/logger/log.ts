c22d6457267827b66ea30559912ce90bfcdf3e70
Julien Bara <julien.bara@360learning.com>
2021-02-25 20:07:03 +0100

    logger

--- src/2021/logger/log.ts
+++ src/2021/logger/log.ts
@@ -1,6 +1,27 @@
+import { fileWriter } from '../../utils';
 import { Submission } from '../entities';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function log(submission: Submission, folder: string, score: number): void {
-    // write output file
+    const submissionFile = buildSubmissionFile(submission);
+    writeFile(folder, score, submissionFile);
+}
+
+function buildSubmissionFile(submission: Submission) : string {
+    let out = '';
+    out += submission.intersectionSchedules.length;
+    out += '\n';
+    for (const intersectionSchedule of submission.intersectionSchedules) {
+        out += intersectionSchedule.intersection;
+        out += '\n';
+        out += intersectionSchedule.schedules.length;
+        out += '\n';
+        intersectionSchedule.schedules.forEach(schedule => {
+            out += schedule.street + ' ' + schedule.duration + '\n';
+        });
+    }
+    return out;
+}
+
+function writeFile(folder: string, score: number, data: string) : void{
+    fileWriter.writeFile(`2021/outputs/${folder}/${score}.txt`, data);
 }
--- src/2021/solver/julien/solve.ts
+++ src/2021/solver/julien/solve.ts
@@ -4,7 +4,28 @@ import { Input, Submission } from '../../entities';
 export function solve(input: Input) : Submission {
     // const x : Submission = {};
 
-    return {intersectionSchedules:[]};
+    return {intersectionSchedules:[{
+        intersection: 1,
+        schedules: [{
+            street: 'rue-d-athenes',
+            duration: 2
+        }, {
+            street: 'rue-d-amsterdam',
+            duration: 1
+        }]
+    },{
+        intersection: 0,
+        schedules: [{
+            street: 'rue-de-londres',
+            duration: 2
+        }]
+    },{
+        intersection: 2,
+        schedules: [{
+            street: 'rue-de-moscou',
+            duration: 1
+        }]
+    }]};
 }
 
 // Python's `range` method
