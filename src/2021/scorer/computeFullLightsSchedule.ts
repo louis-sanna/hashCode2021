52e446f77324988154308eb8748799696f54767e
sebastien-mignot <mignot.sebastien@gmail.com>
2021-02-25 20:35:04 +0100

    computeFullLightsSchedule

---
+++ src/2021/scorer/computeFullLightsSchedule.ts
@@ -0,0 +1,22 @@
+import { FullLightsSchedule, Submission, IntersectionSchedule, StreetSchedule, Input } from '../entities';
+
+export function computeFullLightsSchedule(input: Input, submission: Submission) : FullLightsSchedule {
+    const fullLightsSchedule = <FullLightsSchedule>{
+        greenStreetByTimeAndIntersection: new Array(input.duration)
+    };
+    for (let i = 0; i < input.duration; ++i) {
+        fullLightsSchedule.greenStreetByTimeAndIntersection[i] = new Array(input.intersectionsCount);
+    }
+    submission.intersectionSchedules.forEach(intersectionSchedule => {
+        const streetCycle = [];
+        intersectionSchedule.schedules.forEach(streetSchedule => {
+            for (let i = 0; i < streetSchedule.duration; ++i) {
+                streetCycle.push(streetSchedule.street);
+            }
+        });
+        for (let i = 0; i < input.duration; ++i) {
+            fullLightsSchedule[i][intersectionSchedule.intersection] = streetCycle[i % streetCycle.length];
+        }
+    });
+    return fullLightsSchedule;
+}
