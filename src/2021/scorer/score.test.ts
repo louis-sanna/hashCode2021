1f5bd957536430acbad72dded9275dcca099eee7
Louis Sanna <louis.sanna@360learning.com>
2021-02-26 21:25:51 +0100

    First correct score

--- src/2021/scorer/score.test.ts
+++ src/2021/scorer/score.test.ts
@@ -24,6 +24,58 @@ describe('score', () => {
         expect(totalScore).to.equal(0);
     });
 
+    it('should return 0 if no green lights', () => {
+        const input = {
+            duration: 0,
+            bonus: 0,
+            streets: [{
+                begin: 0,
+                end: 1,
+                name: 'name of street',
+                length: 1
+            }],
+            cars: [{
+                pathStreetNames: ['name of street']
+            }]
+        };
+        const submission = {
+            intersectionSchedules: []
+        };
+
+        const totalScore = score(submission, input);
+
+        expect(totalScore).to.equal(0);
+    });
+
+    it('should return bonus if light is already green', () => {
+        const input = {
+            duration: 0,
+            bonus: 1000,
+            streets: [{
+                begin: 0,
+                end: 1,
+                name: 'name of street',
+                length: 1
+            }],
+            cars: [{
+                pathStreetNames: ['name of street']
+            }]
+        };
+        const submission = {
+            intersectionSchedules: [{
+                intersection: 1,
+                schedules: [{
+                    street: 'name of street',
+                    duration: 10
+                }]
+            }]
+        };
+
+        const totalScore = score(submission, input);
+
+        expect(totalScore).to.equal(1000);
+    });
+
     it.skip('should return 0 input there are no cars', () => {
         const input = {
             duration: 0,
--- src/2021/scorer/score.ts
+++ src/2021/scorer/score.ts
@@ -12,5 +12,65 @@ interface carAdapter {
 }
 
 export function score(submission: Submission, input: InputAdapter): number {
-    return 0;
+    const isGreenByTimeByStreet = buildIsGreenByTimeByStreet(submission, input);
+    const cars = buildCars(input);
+    console.log('isGreenByTimeByStreet', isGreenByTimeByStreet);
+    console.log('cars', cars);
+    const time = 0;
+    cars.forEach(car => updateCarAtIntersection(car, time));
+    console.log('cars', cars);
+    return scoreCars(cars, input);
+}
+
+function scoreCars(cars, input) {
+    let totalScore = 0;
+    for (const car of cars) {
+        if (typeof car.arrived !== 'undefined') {
+            totalScore += input.bonus;
+            totalScore += input.duration - car.arrived;
+        }
+    }
+    return totalScore;
+}
+
+function updateCarAtIntersection(car, time) {
+    if (typeof car.arrived !== 'undefined') {
+        return;
+    }
+    if (car.timeRemainingOnStreet === 0) {
+        if (car.pathStreetNames.length === 1) {
+            car.arrived = time;
+            return;
+        }
+    }
+}
+
+function buildCars(input) {
+    return input.cars.map(inputCar => ({
+        pathStreetNames: inputCar.pathStreetNames,
+        timeRemainingOnStreet: 0
+    }));
+}
+
+function buildIsGreenByTimeByStreet(submission, input) {
+    const isGreenByTimeByStreet = [];
+    for (let virtualTime = 0; virtualTime <= input.duration; virtualTime++) {
+        isGreenByTimeByStreet[virtualTime] = [];
+        for (const street of input.streets) {
+            isGreenByTimeByStreet[virtualTime][street.name] = false;
+        }
+    }
+    for (const intersectionSchedule of submission.intersectionSchedules) {
+        const streetCycle : string[] = [];
+        intersectionSchedule.schedules.forEach(streetSchedule => {
+            for (let i = 0; i <= streetSchedule.duration; i++) {
+                streetCycle.push(streetSchedule.street);
+            }
+            for (let virtualTime = 0; virtualTime <= input.duration; virtualTime++) {
+                const currentGreenStreet = streetCycle[virtualTime % streetCycle.length];
+                isGreenByTimeByStreet[virtualTime][currentGreenStreet] = true;
+            }
+        });
+    }
+    return isGreenByTimeByStreet;
 }
